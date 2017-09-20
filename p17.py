821100b4adb0aa04d2725f8daa762420c5c7812e
abaisa <965718196@qq.com>
2017-09-20 10:17:34 +0800

    problem 17 my solution
    Signed-off-by: abaisa <965718196@qq.com>

--- p17.py
+++ p17.py
@@ -1,6 +1,18 @@
 class Solution(object):
     def letterCombinations(self, digits):
-        dictionary = [[], [], [a, b, c], [d, e, f], [g, h, i], \
-        [j, k. l], [m, n, o], [p, q, r, s], [t, u, v], [w, x, y, z]]
-		
-		return dictionary[digits]        
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dictionaryInfo = [[], [], ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
+        res = []
+        for i in range(len(digits)):
+            if res == []:
+                res = dictionaryInfo[int(digits[i])]
+            else:
+                newRes = []
+                for j in range(len(res)):
+                    for k in range(len(dictionaryInfo[int(digits[i])])):
+                        newRes.append(res[j] + dictionaryInfo[int(digits[i])][k])
+                res = newRes
+        return res
