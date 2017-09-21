9996cdf055397b68a2c53516344742fcb85ec41e
abaisa <965718196@qq.com>
2017-09-21 14:56:58 +0800

    problem 21 solution and note
    Signed-off-by: abaisa <965718196@qq.com>

---
+++ 21.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if(l1 == None):
+            return l2
+        if(l2 == None):
+            return l1
+        
+        head = ListNode(0)
+        curr = head
+        while(l1 and l2):
+            if(l1.val < l2.val):
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        if(l1):
+            curr.next = l1
+        else:
+            curr.next = l2
+        
+        return head.next
---
+++ 21model.py
@@ -0,0 +1,51 @@
+# iteratively
+def mergeTwoLists1(self, l1, l2):
+    dummy = cur = ListNode(0)
+    while l1 and l2:
+        if l1.val < l2.val:
+            cur.next = l1
+            l1 = l1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+    cur.next = l1 or l2
+    return dummy.next
+    
+# recursively    
+def mergeTwoLists2(self, l1, l2):
+    if not l1 or not l2:
+        return l1 or l2
+    if l1.val < l2.val:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = self.mergeTwoLists(l1, l2.next)
+        return l2
+        
+# in-place, iteratively        
+def mergeTwoLists(self, l1, l2):
+    if None in (l1, l2):
+        return l1 or l2
+    dummy = cur = ListNode(0)
+    dummy.next = l1
+    while l1 and l2:
+        if l1.val < l2.val:
+            l1 = l1.next
+        else:
+            nxt = cur.next
+            cur.next = l2
+            tmp = l2.next
+            l2.next = nxt
+            l2 = tmp
+        cur = cur.next
+    cur.next = l1 or l2
+    return dummy.next
+
+'''
+1.常规迭代，基本我自己写的差不多。
+需要注意的是，cur.next = l1 or l2
+这个使用很简单，看一下
+2.递归写法。没啥好说的
+3.迭代写法。
+插入方式写的，不占用额外空间，只调整‘next指针’
